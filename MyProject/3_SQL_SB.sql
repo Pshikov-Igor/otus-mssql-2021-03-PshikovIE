f80a041b2f0bc9cda7ef137dec3798114f0dc5b5
Pshikov-Igor <pshikovie@gmail.com>
2021-08-13 16:04:38 +0300

    изм

--- MyProject/3_SQL_SB.sql
+++ MyProject/3_SQL_SB.sql
@@ -243,7 +243,7 @@ END
 
 ------------------------------------------------------------------------------------------------------------------------
 --Автоматизируем 
-
+--Создание очередей
 ALTER QUEUE sbyt.[InitiatorQueueSbyt] WITH STATUS = ON , RETENTION = OFF , POISON_MESSAGE_HANDLING (STATUS = OFF) 
 	, ACTIVATION (   STATUS = ON ,
         PROCEDURE_NAME = sbyt.proc_write_journal_SB_ConfirmMessage, MAX_QUEUE_READERS = 1, EXECUTE AS OWNER) ; 
